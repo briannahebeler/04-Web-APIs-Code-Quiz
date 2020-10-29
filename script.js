e7c43cad7c3eaa68b7f2fb133bc945b0b7e902f2
Brianna Hebeler <briannahebeler@gmail.com>
2020-10-29 19:34:38 -0400

    created outline for questions var/arr/object

---
+++ script.js
@@ -0,0 +1,7 @@
+var questions = [
+    { question: "This will be the first question?", choices: ["choice one", "choice two", "choice three", "choice four"], correctAnswer: 1 },
+    { question: "This will be the second question?", choices: ["choice one", "choice two", "choice three", "choice four"], correctAnswer: 4 },
+    { question: "This will be the third question?", choices: ["choice one", "choice two", "choice three", "choice four"], correctAnswer: 2 },
+    { question: "This will be the fourth question?", choices: ["choice one", "choice two", "choice three", "choice four"], correctAnswer: 2 },
+    { question: "This will be the fifth question?", choices: ["choice one", "choice two", "choice three", "choice four"], correctAnswer: 3 }
+];
