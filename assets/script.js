7dcb9923db89da6c9dc5ba299b1d1137eccf3927
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-07 12:21:41 -0500

    created functions to display/ block certain sections of index

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,38 @@
 var questions = [
     { q: "This will be the first question?", 
-    mc:["choice 1", "choice 2", "choice 3", "choice 4"],
-    a: "choice 4"},
+    mc:["a", "b", "c", "d"],
+    a: "d"},
     { q: "This will be the second question?", 
-    mc:["choice 1", "choice 2", "choice 3", "choice 4"],
-    a: "choice 1"},
+    mc:["a", "b", "c", "d"],
+    a: "a"},
     { q: "This will be the third question?",
-    mc:["choice 1", "choice 2", "choice 3", "choice 4"],
-    a: "choice 2"},
+    mc:["a", "b", "c", "d"],
+    a: "b"},
     { q: "This will be the fourth question?",
-    mc:["choice 1", "choice 2", "choice 3", "choice 4"],
-    a: "choice 2"},
+    mc:["a", "b", "c", "d"],
+    a: "b"},
     { q: "This will be the fifth question?",
-    mc:["choice 1", "choice 2", "choice 3", "choice 4"],
-    a: "choice 3"},
+    mc:["a", "b", "c", "d"],
+    a: "c"},
 ];
 
 // variables from index.html
 var timer = document.querySelector("#timer");
 var jumbo = document.querySelector("#jumbotron");
 var startBtn = document.querySelector("#start-btn");
+
+//variables from quiz box
 var quizBox = document.getElementById("quiz-box");
+var questions = document.getElementById("questions");
+var choices = document.getElementById("choices");
 var choiceA = document.getElementById("#a");
 var choiceB = document.getElementById("#b");
 var choiceC = document.getElementById("#c");
 var choiceD = document.getElementById("#d");
+var quizNavBtns = document.getElementById("quiz-nav-btns");
+var backBtn = document.getElementById("back");
+var nextBtn = document.getElementById("next");
+var submitQuizBtn = document.getElementById("submit-quiz");
 
 // variables from initials box
 var initialsBox = document.getElementById("#initials-box");
@@ -36,8 +44,8 @@ var initialSubmitBtn = document.getElementById("#submit-initials");
 // variables from highscores.html
 var highScoresContainer = document.getElementById("results");
 var highScores = document.querySelector("#highscores");
-var UserScores = document.querySelector("#user-scores");
-var GoBackBtn = document.querySelector("#go-back");
+var userScores = document.querySelector("#user-scores");
+var goBackBtn = document.querySelector("#go-back");
 
 // this variable sets the timer to 100 seconds - giving 20 seconds per question
 var secondsLeft = 100;
@@ -61,30 +69,51 @@ function sendMessage() {
     // send an alert that the quiz is now over & show initials box
 }
 
+
+//this function will display quiz box
+function showQuizBox() {
+    if (jumbo.style.display === "block" && quizBox.style.display === "none") {
+        jumbo.style.display = "none";
+        quizBox.style.display = "block";
+    }
+}
+
+function startQuiz () {
+    // var currentQuestion = 0;
+    // var score = 0;
+
+    // function displayCurrentQuestion(question) {
+    //     for (var i = 0; i < questions.length; i++) {
+    //         var currentQ = questions[i].q;
+    //         console.log(currentQ);
+    //     }
+    // }
+
+    // displayCurrentQuestion(0);
+}
+
+//this function will display initial box
+function showInitialBox () {
+    if (jumbo.style.display === "none" && quizBox.style.display === "block" && initialsBox.style.display === "none") {
+        jumbo.style.display = "none";
+        quizBox.style.display = "none";
+        initialsBox.style.display = "block"
+    } 
+}
+
+// this function will bring the user to highscores page
+function showHighScores() {
+
+}
+
+// EVENT LISTENERS
+// ====================================================
 //timer will start when press start button
 startBtn.addEventListener("click", setTime);
 //quiz box will display when press start button
-startBtn.addEventListener("click", startQuiz);
-
-
-
-//this function will display & start quiz
-function startQuiz() {
-    var currentQuestion = 0;
-    var score = 0;
-
-    function displayCurrentQuestion(question) {
-        for (var i = 0; i < questions.length; i++) {
-            var currentQ = questions[i].q;
-            console.log(currentQ);
-        }
-    }
-
-    displayCurrentQuestion(0);
-}
-
-
-// this function will bring the user to highscores page
-function showHighScores() {}
-
+startBtn.addEventListener("click", showQuizBox);
+//when press submit quiz button it will display initial box
+submitQuizBtn.addEventListener("click", showInitialBox)
+//when press initial submit button it will show high score page
 initialSubmitButton.addEventListener("click", showHighScores);
+// ====================================================
