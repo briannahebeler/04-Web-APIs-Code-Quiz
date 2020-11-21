fa50d3734343833c152a77784e4956bbaa6eb7ef
Brianna Hebeler <briannahebeler@gmail.com>
2020-11-21 01:10:08 -0500

    got high scores to display according to what is saved in local storage... still wokring on adding the items into an array rather then having it refresh each time a user saves

--- assets/script.js
+++ assets/script.js
@@ -83,9 +83,6 @@ var userAnswer;
 //used to keep track of questions 
 var index = 0;
 
-var savedInitials = [];
-var savedScores = [];
-
 
 // ===================================================
 // FUNCTIONS
@@ -173,23 +170,39 @@ function showInitialBox() {
     }
 
     document.getElementById("your-score").innerHTML = "Your Score: " + yourScore;
-    //push your score to saved scores array
+}
+
+var savedInitials = [];
+var savedScores = [];
+
+// this function data to local storage
+function saveData() {
+
+    //SAVING SCORE//push your score to saved scores array, then save array to LS
     savedScores.push(yourScore);
     localStorage.setItem("userScore", savedScores);
-}
 
-// // this function saves the data to the local storage
-function saveData() {
+
+    //SAVING INITIALS//
     var enteredInitials = document.getElementById("myInput").value;
     savedInitials.push(enteredInitials);
-    document.localStorage.setItem("userInitials", savedInitials);
+    localStorage.setItem("userInitials", savedInitials);
 }
 
-// const userInitials = document.getElementById("user-initials");
-// const userScores = document.getElementById("user-scores");
+const userInitials = document.getElementById("user-initials");
+const userScores = document.getElementById("user-scores");
 
 function displayHighScores() {
 
+    // for (var i = 0; i < savedInitials.length; i++) {
+        userInitials.innerHTML = localStorage.getItem("userInitials");
+        console.log(localStorage.getItem("userInitials"));
+    // }
+
+    // for (var i = 0; i < savedScores.length; i++) {
+        userScores.innerHTML = localStorage.getItem("userScore");
+        console.log(localStorage.getItem("userScore"));
+    // }
 }
 
 // ===================================================
